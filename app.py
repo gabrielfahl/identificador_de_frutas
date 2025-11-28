ef5d602757237dafed1f93557e5ddecaa7c8031f
gabriel <biel_fahl@hotmail.com>
2025-11-28 16:58:36 -0300

    ajuste app.py

--- app.py
+++ app.py
@@ -13,3 +13,7 @@ async def predict(file: UploadFile = File(...)):
 @app.get("/")
 async def read_index():
     return FileResponse("index.html")
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
